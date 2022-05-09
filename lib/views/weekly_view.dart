7a989de2fd24dc0f26fd3e776f2b88963b00e776
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2022-05-09 20:29:15 +0900

    週間天気予報画面

--- lib/views/weekly_view.dart
+++ lib/views/weekly_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 class WeeklyView extends StatelessWidget {
   const WeeklyView({Key? key}) : super(key: key);
@@ -6,21 +7,45 @@ class WeeklyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              "週間天気",
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200.0,
+            title: Text(
+              "札幌市中央区",
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+                fontSize: 25,
               ),
             ),
-          ],
-        ),
+            backgroundColor: Colors.black,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    BoxedIcon(
+                      WeatherIcons.day_sunny,
+                      size: 50,
+                    ),
+                    Text(
+                      "晴れ",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.grey.shade300,
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
